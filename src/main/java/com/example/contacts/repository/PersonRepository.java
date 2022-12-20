155bf239dd4df1b47566ae26cbc1c0626a2676fd
秋田 みのり <minori.akita@PTCSPC0328.ptcs-lab.local>
2022-12-20 15:25:29 +0900

    techpit springboot

---
+++ src/main/java/com/example/contacts/repository/PersonRepository.java
@@ -0,0 +1,12 @@
+package com.example.contacts.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.contacts.models.Person;
+
+public interface PersonRepository extends JpaRepository<Person, Long> {
+
+}
+
+// public class PersonRepository {
+    
+// }
